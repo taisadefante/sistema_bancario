32d8eb13146adb2fed88627f01517606c98c68ca
tais defante <taisadefante@hotmail.com>
2024-01-27 15:31:43 -0300

    novo projeto

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({ error: "Customer already exists!" });
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return res.status(201).send();
+});
+
+app.listen(3000);
