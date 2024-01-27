650a8d6cca8e6cae1cb34e418030a070f64a9399
tais defante <taisadefante@hotmail.com>
2024-01-27 16:24:54 -0300

    alteração projeto

--- src/index.js
+++ src/index.js
@@ -30,11 +30,14 @@ app.post("/account", (req, res) => {
 });
 
 //buscar extrato cliente
-app.get("/statement/:cpf", (req, res) => {
-  const { cpf } = req.params;
+app.get("/statement", (req, res) => {
+  const { cpf } = req.headers;
 
   const customer = customers.find((customer) => customer.cpf === cpf);
 
+  if (!customer) {
+    return res.status(400).json({ error: "Customer not found" });
+  }
   return res.json(customer.statement);
 });
 
